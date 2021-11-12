2a2ef5b458598140f7bd115bff958a4316a2647a
Wilmer Escobar <es.wilmer93@gmail.com>
2021-11-12 04:00:33 -0500

    Eureka

--- bike-service/src/main/java/com/demo/microservices/BikeServiceApplication.java
+++ bike-service/src/main/java/com/demo/microservices/BikeServiceApplication.java
@@ -2,8 +2,10 @@ package com.demo.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BikeServiceApplication {
 
 	public static void main(String[] args) {
--- eureka-service/src/main/java/com/demo/microservices/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/demo/microservices/EurekaServiceApplication.java
@@ -2,14 +2,14 @@ package com.demo.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class CarServiceApplication {
+@EnableEurekaServer
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
